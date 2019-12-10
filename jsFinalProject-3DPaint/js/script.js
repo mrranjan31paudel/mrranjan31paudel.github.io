0541ebfebde09a27017a5bcbc744471a07b0a48d
mrranjan31paudel <paudeljimrranjan@gmail.com>
2019-12-10 15:27:15 +0545

    3DPaint 3rd

--- jsFinalProject-3DPaint/js/script.js
+++ jsFinalProject-3DPaint/js/script.js
@@ -35,50 +35,55 @@
       var varDepth = this.depth //- this.positionPoint[2]/this.factor;
 
       context.beginPath();
-      context.moveTo(this.point_FLT[0] , this.point_FLT[1]);// - this.point_FLT[2]/this.factor + this.point_FLT[2]/this.factor
-      context.lineTo(this.point_DLT[0] , this.point_DLT[1]);// - this.point_DLT[2]/this.factor + this.point_DLT[2]/this.factor
-      context.lineTo(this.point_DLB[0] , this.point_DLB[1]);// - this.point_DLB[2]/this.factor + this.point_DLB[2]/this.factor
-      context.lineTo(this.point_FLB[0] , this.point_FLB[1]);// - this.point_FLB[2]/this.factor + this.point_FLB[2]/this.factor
-      context.lineTo(this.point_FLT[0] , this.point_FLT[1]);// - this.point_FLT[2]/this.factor + this.point_FLT[2]/this.factor
-      context.lineTo(this.point_FRT[0] , this.point_FRT[1]);// - this.point_FRT[2]/this.factor + this.point_FRT[2]/this.factor
-      context.lineTo(this.point_FRB[0] , this.point_FRB[1]);// - this.point_FRB[2]/this.factor + this.point_FRB[2]/this.factor
-      context.lineTo(this.point_DRB[0] , this.point_DRB[1]);// - this.point_DRB[2]/this.factor + this.point_DRB[2]/this.factor  
-      context.lineTo(this.point_DRT[0] , this.point_DRT[1]);// - this.point_DRT[2]/this.factor + this.point_DRT[2]/this.factor
-      context.lineTo(this.point_FRT[0] , this.point_FRT[1]);// - this.point_FRT[2]/this.factor + this.point_FRT[2]/this.factor
-      context.moveTo(this.point_DLT[0] , this.point_DLT[1]);// - this.point_DLT[2]/this.factor + this.point_DLT[2]/this.factor
-      context.lineTo(this.point_DRT[0] , this.point_DRT[1]);// - this.point_DRT[2]/this.factor + this.point_DRT[2]/this.factor
-      context.moveTo(this.point_DLB[0] , this.point_DLB[1]);// - this.point_DLB[2]/this.factor + this.point_DLB[2]/this.factor
-      context.lineTo(this.point_DRB[0] , this.point_DRB[1]);// - this.point_DRB[2]/this.factor + this.point_DRB[2]/this.factor
-      context.moveTo(this.point_FLB[0] , this.point_FLB[1]);// - this.point_FLB[2]/this.factor + this.point_FLB[2]/this.factor
-      context.lineTo(this.point_FRB[0] , this.point_FRB[1]);// - this.point_FRB[2]/this.factor + this.point_FRB[2]/this.factor
-      // context.moveTo(this.point_centroidZ[0] + this.point_centroidZ[2]/this.factor, this.point_centroidZ[1] - this.point_centroidZ[2]/this.factor);
-      // context.lineTo(this.point_centroidZ[0] + this.point_centroidZ[2]/this.factor +1, this.point_centroidZ[1]- this.point_centroidZ[2]/this.factor +1);
+      context.moveTo(this.point_FLT[0] , this.point_FLT[1]);
+      context.lineTo(this.point_DLT[0] , this.point_DLT[1]);
+      context.lineTo(this.point_DLB[0] , this.point_DLB[1]);
+      context.lineTo(this.point_FLB[0] , this.point_FLB[1]);
+      context.lineTo(this.point_FLT[0] , this.point_FLT[1]);
+      context.lineTo(this.point_FRT[0] , this.point_FRT[1]);
+      context.lineTo(this.point_FRB[0] , this.point_FRB[1]);
+      context.lineTo(this.point_DRB[0] , this.point_DRB[1]); 
+      context.lineTo(this.point_DRT[0] , this.point_DRT[1]);
+      context.lineTo(this.point_FRT[0] , this.point_FRT[1]);
+      context.moveTo(this.point_DLT[0] , this.point_DLT[1]);
+      context.lineTo(this.point_DRT[0] , this.point_DRT[1]);
+      context.moveTo(this.point_DLB[0] , this.point_DLB[1]);
+      context.lineTo(this.point_DRB[0] , this.point_DRB[1]);
+      context.moveTo(this.point_FLB[0] , this.point_FLB[1]);
+      context.lineTo(this.point_FRB[0] , this.point_FRB[1]);
+ 
       context.stroke();
       
     }
-
-    this.stretchCube = function(){
-      
-      var posX = this.positionPoint[0];
-      var posY = this.positionPoint[1];
-      var posZ = this.positionPoint[2];
-
-      var varWidth = this.width;// - this.positionPoint[2]/4;
-      var varHeight = this.height;// - this.positionPoint[2]/4;
-      var varDepth = this.depth //- this.positionPoint[2]/4;
-
-      this.point_FLT = [posX, posY, posZ];
-      this.point_FRT = [posX + varWidth, posY, posZ];
-      this.point_FLB = [posX, posY + varHeight, posZ];
-      this.point_FRB = [posX + varWidth, posY + varHeight, posZ];
-      this.point_DLT = [posX, posY, posZ + varDepth];
-      this.point_DRT = [posX + varWidth, posY, posZ + varDepth];
-      this.point_DLB = [posX, posY + varHeight, posZ + varDepth];
-      this.point_DRB = [posX + varWidth, posY + varHeight, posZ + varDepth];
+// ***********************************************************Stretch***********
+    this.stretchCubeXdir = function(xShift){
+      context.clearRect(0, 0 , 400, 400);
+      // this.point_FLT[0] = this.point_FLT[0] + xShift;
+      this.point_FRT[0] = this.point_FRT[0] + xShift;
+      // this.point_FLB[0] = this.point_FLB[0] + xShift;
+      this.point_FRB[0] = this.point_FRB[0] + xShift;
+      // this.point_DLT[0] = this.point_DLT[0] + xShift;
+      this.point_DRT[0] = this.point_DRT[0] + xShift;
+      // this.point_DLB[0] = this.point_DLB[0] + xShift;
+      this.point_DRB[0] = this.point_DRB[0] + xShift;
 
       this.drawCube();
     }
 
+    // this.stretchCubeYdir = function(){
+
+    //   this.point_FLT = ;
+    //   this.point_FRT = ;
+    //   this.point_FLB = ;
+    //   this.point_FRB = ;
+    //   this.point_DLT = ;
+    //   this.point_DRT = ;
+    //   this.point_DLB = ;
+    //   this.point_DRB = ;
+
+    //   this.drawCube();
+    // }
+// ****************************************************************Rotation****
     this.rotateCubeAbtY = function(radAngle){
       
       var pi = Math.PI;
@@ -86,8 +91,8 @@
       var sinShift = Math.sin(radAngle);
       var cosShift = Math.cos(radAngle);
 
-      // this.positionPoint[0] = relativeX * Math.sin(radAngle);
-      // this.positionPoint[2] = relativeZ * Math.cos(radAngle);  
+      // this.positionPoint[0] = this.point_centroidY[0]+(this.positionPoint[0]-this.point_centroidY[0])*cosShift - (this.positionPoint[2]-this.point_centroidY[2])*sinShift;
+      // this.positionPoint[2] = this.point_centroidY[2]+(this.positionPoint[2]-this.point_centroidY[2])*cosShift + (this.positionPoint[0]-this.point_centroidY[0])*sinShift;  
       context.clearRect(0, 0, 400, 400);
       this.point_FLT = [ this.point_centroidY[0]+(this.point_FLT[0]-this.point_centroidY[0])*cosShift - (this.point_FLT[2]-this.point_centroidY[2])*sinShift, this.point_FLT[1],  this.point_centroidY[2]+(this.point_FLT[2]-this.point_centroidY[2])*cosShift + (this.point_FLT[0]-this.point_centroidY[0])*sinShift];
       this.point_DLT = [ this.point_centroidY[0]+(this.point_DLT[0]-this.point_centroidY[0])*cosShift - (this.point_DLT[2]-this.point_centroidY[2])*sinShift, this.point_DLT[1],  this.point_centroidY[2]+(this.point_DLT[2]-this.point_centroidY[2])*cosShift + (this.point_DLT[0]-this.point_centroidY[0])*sinShift];
@@ -97,7 +102,6 @@
       this.point_DLB = [ this.point_centroidY[0]+(this.point_DLB[0]-this.point_centroidY[0])*cosShift - (this.point_DLB[2]-this.point_centroidY[2])*sinShift, this.point_DLB[1],  this.point_centroidY[2]+(this.point_DLB[2]-this.point_centroidY[2])*cosShift + (this.point_DLB[0]-this.point_centroidY[0])*sinShift];
       this.point_FRB = [ this.point_centroidY[0]+(this.point_FRB[0]-this.point_centroidY[0])*cosShift - (this.point_FRB[2]-this.point_centroidY[2])*sinShift, this.point_FRB[1],  this.point_centroidY[2]+(this.point_FRB[2]-this.point_centroidY[2])*cosShift + (this.point_FRB[0]-this.point_centroidY[0])*sinShift];
       this.point_DRB = [ this.point_centroidY[0]+(this.point_DRB[0]-this.point_centroidY[0])*cosShift - (this.point_DRB[2]-this.point_centroidY[2])*sinShift, this.point_DRB[1],  this.point_centroidY[2]+(this.point_DRB[2]-this.point_centroidY[2])*cosShift + (this.point_DRB[0]-this.point_centroidY[0])*sinShift];
-
       this.drawCube();
     }
 
@@ -142,6 +146,21 @@
 
       this.drawCube();
     }
+
+// ****************************************************************Move or shift
+    this.moveCube = function(xShift, yShift){
+      context.clearRect(0, 0 , 400, 400);
+      this.point_FLT = [this.point_FLT[0] + xShift, this.point_FLT[1] + yShift, this.point_FLT[2]];
+      this.point_FRT = [this.point_FRT[0] + xShift, this.point_FRT[1] + yShift, this.point_FRT[2]];
+      this.point_FLB = [this.point_FLB[0] + xShift, this.point_FLB[1] + yShift, this.point_FLB[2]];
+      this.point_FRB = [this.point_FRB[0] + xShift, this.point_FRB[1] + yShift, this.point_FRB[2]];
+      this.point_DLT = [this.point_DLT[0] + xShift, this.point_DLT[1] + yShift, this.point_DLT[2]];
+      this.point_DRT = [this.point_DRT[0] + xShift, this.point_DRT[1] + yShift, this.point_DRT[2]];
+      this.point_DLB = [this.point_DLB[0] + xShift, this.point_DLB[1] + yShift, this.point_DLB[2]];
+      this.point_DRB = [this.point_DRB[0] + xShift, this.point_DRB[1] + yShift, this.point_DRB[2]];
+      this.drawCube();
+    }
+    
   }
 
     function MainFunc(parentContainer, newCanvas){
@@ -157,14 +176,21 @@
       
       var cube = new Cube(context, pos, ht, wd, dt);
       cube.drawCube();
-      var angle = 0.01745 * 45;
-      cube.rotateCubeAbtX(angle);
-      cube.rotateCubeAbtY(angle);
-      cube.rotateCubeAbtZ(angle);
-      // setInterval(function(){
-      //   // context.clearRect(0, 0, 400, 400);
-      //   cube.rotateCubeAbtX(angle);
-      // }, 20)    
+      var angle = 0.01745;
+
+      // cube.stretchCubeXdir(40);
+      // cube.rotateCubeAbtX(angle*45);
+      // cube.rotateCubeAbtY(-angle*45)
+      // cube.rotateCubeAbtZ(angle*45);
+      // cube.stretchCubeXdir(40);
+
+      setInterval(function(){
+        // context.clearRect(0, 0, 400, 400);
+        cube.rotateCubeAbtX(angle);
+        cube.rotateCubeAbtY(-angle);
+        cube.rotateCubeAbtZ(angle);
+        // cube.moveCube(1, 1);
+      }, 20)    
 
     }
 
